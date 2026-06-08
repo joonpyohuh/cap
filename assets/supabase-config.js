9577c1a1deaedbad63e47834521db1f2275e5634
joonpyohuh <joonpyohuh@users.noreply.github.com>
2026-06-08 12:54:33 +0900

    Configure Supabase project

--- assets/supabase-config.js
+++ assets/supabase-config.js
@@ -1,5 +1,5 @@
 window.PLAYPICK_SUPABASE = {
-  url: "",
-  anonKey: "",
+  url: "https://yxuxokgrngttlqniorof.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4dXhva2dybmd0dGxxbmlvcm9mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4ODI5MDgsImV4cCI6MjA5NjQ1ODkwOH0.xlqzznSKULDmPO-uLDb_bYR1uJGwp1h6LAN3gHm1qvQ",
   currentUserId: "U-1001"
 };
